e36ea02dd342ff10e5ec4d86b7d00dee519b5c79
liboshuai001 <96489281+liboshuai001@users.noreply.github.com>
2022-05-12 09:06:26 +0800

    添加jpa修改、删除、查询方法测试，并使用maven重新编译打包项目

--- 01-jpa-hibernate/src/test/java/SpringDataJapTest.java
+++ 01-jpa-hibernate/src/test/java/SpringDataJapTest.java
@@ -29,4 +29,36 @@ public class SpringDataJapTest {
                 .build();
         customerDao.save(build);
     }
+
+    /**
+     * 修改客户：调用save(obj)方法
+     * 对于 save 方法的解释：如果执行此方法是对象中存在id属性，即为更新操作会先根据id查询，再更新
+     * 如果执行此方法中对象中不存在id属性，即为保存操作
+     */
+    @Test
+    public void testUpdate() {
+        // 根据 id 查询客户
+        CustomerEntity customerDaoOne = customerDao.findOne(3L);
+        // 修改客户名称
+        customerDaoOne.setCustName("测试修改客户名称");
+        // 更新操作
+        customerDao.save(customerDaoOne);
+    }
+
+    /**
+     * 根据 id 删除：调用 delete(id)方法
+     */
+    @Test
+    public void testDelete() {
+        customerDao.delete(4L);
+    }
+
+    /**
+     * 根据id查询：调用findOne(id)方法
+     */
+    @Test
+    public void testFindById() {
+        CustomerEntity customerEntity = customerDao.findOne(5L);
+        System.out.println(customerEntity);
+    }
 }
